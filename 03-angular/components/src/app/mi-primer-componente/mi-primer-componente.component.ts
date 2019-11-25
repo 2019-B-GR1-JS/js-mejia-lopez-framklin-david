cfb4832b45569c9c8426e6aa8b00d029e841218e
6N <franklin.mejia@epn.edu.ec>
2019-11-25 18:06:09 -0500

    Componentes 2

--- 03-angular/components/src/app/mi-primer-componente/mi-primer-componente.component.ts
+++ 03-angular/components/src/app/mi-primer-componente/mi-primer-componente.component.ts
@@ -1,22 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-mi-primer-componente',
   templateUrl: './mi-primer-componente.component.html',
   styleUrls: ['./mi-primer-componente.component.scss']
 })
-export class MiPrimerComponenteComponent implements OnInit {
+export class MiPrimerComponenteComponent
+  implements OnInit, OnDestroy {
 
-  constructor() { }
+  @Input()
+  public titulo:string;
 
-  ngOnInit() {
+  @Input()
+  public imagen:string;
+
+  @Input()
+  public textoImagen:string;
+
+  @Input()  // Atributo Propiedad
+  public textoBoton:string;
+  public tamano:string;
+
+  @Output() // EVENTO
+  public cambioSueldo = new EventEmitter();
+  public cambioImagen = new EventEmitter();
+
+  constructor() {
+    console.log('Instanciando');
+  } // instancia la clase (Ayuda -> Angular)
+
+  ngOnInit(): void { // La clase está lista
+    console.log('Esta listo');
+    console.log(this.titulo);
+    console.log(this.textoImagen);
+    console.log(this.textoBoton);
+  }
+
+  ngOnDestroy(): void { // La clase esta destruida
   }
 
   saludar(){
-    alert("Holaaaaaaaaaaaa");
+    alert("HOLAAAAAAAAAAAAAAA");
+  }
+
+  aumentarSueldo(){
+    // this.textoBoton = this.textoBoton + 1;
+    this.textoBoton = (Number(this.textoBoton) + 1).toString();
+    this.cambioSueldo.emit(this.textoBoton);
+    this.cambioImagen.emit(this.tamano);
+    // this.textoBoton = (+this.textoBoton + 1).toString();
   }
 
 
-
-
 }
+
+
+// class (etiqueta html) (web-component)
+// Instanciar a la clase
+// Clase esta lista
+// Clase se destruye
+
+
